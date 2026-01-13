cdbc13bb4009f1e3885bf65d8237ea979dca5e89
Ankita Mane <maneankita831@gmail.com>
2026-01-13 13:03:27 +0530

    12-1-2026 practical

---
+++ Arithmetic.cs
@@ -0,0 +1,10 @@
+class Arithmetic{
+    public static void Main(){
+        int a = 10;
+        int b = 20;
+        System.Console.WriteLine("Addition:"+(a+b));
+        System.Console.WriteLine("Subtraction:"+(a-b));
+        System.Console.WriteLine("Multiplication:"+(a*b));
+        System.Console.WriteLine("Division:"+(a/b));
+    }
+}
